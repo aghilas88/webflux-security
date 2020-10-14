9c707aa50ea894f1d68ad031bc3194cce2d55439
Aghilas DJEMA <aghilas@mbp-de-aghilas.home>
2020-10-14 22:24:48 +0200

    add security config

---
+++ src/main/java/com/agh/webfluxmongo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.agh.webfluxmongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin().disable()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+}
--- src/main/java/com/agh/webfluxmongo/controllers/ProductController.java
+++ src/main/java/com/agh/webfluxmongo/controllers/ProductController.java
@@ -3,17 +3,22 @@ package com.agh.webfluxmongo.controllers;
 import com.agh.webfluxmongo.dto.ProductDto;
 import com.agh.webfluxmongo.model.CreationRequest;
 import com.agh.webfluxmongo.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.server.EntityResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
 import java.util.Date;
+import org.slf4j.Logger;
 
 @RestController
 public class ProductController {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     final ProductRepository productRepository;
 
@@ -21,13 +26,21 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
+    @GetMapping("/infos")
+    public Mono<ResponseEntity<String>> infos(Principal principal){
+        log.info("getAllProducts - Authenticated user : " + principal.getName());
+        return Mono.just(ResponseEntity.ok("product service ok, you are signed in as " + principal.getName()));
+    }
+
     @GetMapping("/products")
-    public Flux<ProductDto> getAllProducts() {
+    public Flux<ProductDto> getAllProducts(Principal principal) {
+        log.info("getAllProducts - Authenticated user : " + principal.getName());
         return productRepository.findAll();
     }
 
     @PostMapping("/products")
-    public Mono<ProductDto> createProduct(@RequestBody CreationRequest creationRequest) {
+    public Mono<ProductDto> createProduct(@RequestBody CreationRequest creationRequest, Principal principal) {
+        log.info("createProduct - Authenticated user : " + principal.getName());
         return productRepository.save(ProductDto.builder()
                 .name(creationRequest.getName())
                 .description(creationRequest.getDescription())
@@ -35,7 +48,8 @@ public class ProductController {
     }
 
     @GetMapping("/products/{id}")
-    public Mono<ResponseEntity<ProductDto>> getProductById(@PathVariable(value = "id") String productId) {
+    public Mono<ResponseEntity<ProductDto>> getProductById(@PathVariable(value = "id") String productId, Principal principal) {
+        log.info("getProductById - Authenticated user : " + principal.getName());
         return productRepository.findById(productId)
                 .map(product -> ResponseEntity.ok(product))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
@@ -43,7 +57,8 @@ public class ProductController {
 
     // Products are sent to the client as Server Sent Events
     @GetMapping(value = "/stream/products", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ProductDto> streamAllTweets() {
+    public Flux<ProductDto> streamAllTweets(Principal principal) {
+        log.info("streamAllTweets - Authenticated user : " + principal.getName());
         return productRepository.findAll();
     }
 
